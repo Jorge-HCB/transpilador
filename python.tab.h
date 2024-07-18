b6c4f3c5f4461972e1a88735a69795d5781aa1c1
Jorge Cavalcante <61051486+Jorge-HCB@users.noreply.github.com>
2024-07-18 13:39:09 -0300

    Add files via upload

---
+++ python.tab.h
@@ -0,0 +1,105 @@
+/* A Bison parser, made by GNU Bison 3.8.2.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
+   Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
+
+#ifndef YY_YY_PYTHON_TAB_H_INCLUDED
+# define YY_YY_PYTHON_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token kinds.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    ABREP = 258,                   /* ABREP  */
+    FECHAP = 259,                  /* FECHAP  */
+    ID = 260,                      /* ID  */
+    STR = 261,                     /* STR  */
+    NUM = 262,                     /* NUM  */
+    FIM_ENTRADA = 263,             /* FIM_ENTRADA  */
+    DOISP = 264,                   /* DOISP  */
+    WHILE = 265,                   /* WHILE  */
+    DO = 266,                      /* DO  */
+    SWITCH = 267,                  /* SWITCH  */
+    CASE = 268,                    /* CASE  */
+    DEFAULT = 269,                 /* DEFAULT  */
+    DIFERENTEIGUAL = 270,          /* DIFERENTEIGUAL  */
+    IGUALIGUAL = 271,              /* IGUALIGUAL  */
+    IGUAL = 272,                   /* IGUAL  */
+    MAIS = 273,                    /* MAIS  */
+    MENOS = 274,                   /* MENOS  */
+    VEZES = 275,                   /* VEZES  */
+    IDENTA = 276,                  /* IDENTA  */
+    FIM_DE_LINHA = 277,            /* FIM_DE_LINHA  */
+    DIVIDIR = 278                  /* DIVIDIR  */
+  };
+  typedef enum yytokentype yytoken_kind_t;
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+union YYSTYPE
+{
+#line 12 "python.y"
+
+    char *str;
+
+#line 91 "python.tab.h"
+
+};
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+
+int yyparse (void);
+
+
+#endif /* !YY_YY_PYTHON_TAB_H_INCLUDED  */
